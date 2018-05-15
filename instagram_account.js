5f17450042adeb368ca09ab07104474d081b1ac0
Jason Adkison <jadkison@gmail.com>
2018-05-15 12:36:44 -0500

    Update instagram_account.js

--- instagram_account.js
+++ instagram_account.js
@@ -3,12 +3,16 @@
 "phantombuster command: nodejs"
 "phantombuster package: 5"
 "phantombuster flags: save-folder"
+"phantombuster dependencies: lib-Sponsored.js"
 
 const Buster = require("phantombuster")
 const buster = new Buster()
 
 const Nick = require("nickjs")
 const nick = new Nick()
+
+const _ = require("lodash")
+const Sponsored = require("./lib-Sponsored")
 
 // }
 
@@ -19,14 +23,14 @@ nick.newTab().then(async (tab) => {
   await tab.untilVisible("#react-root > section > main header:first-of-type section:first-of-type h1:first-of-type"); // Make sure we have loaded the page
   await tab.inject("../injectables/jquery-3.0.0.min.js"); // We're going to use jQuery to scrape
   await tab.inject("../injectables/lodash-full-4.13.1.min.js"); // We're going to use lodash to extract certain data from graphql structure
-  const account = await tab.evaluate((arg, callback) => {
+  return await tab.evaluate((arg, callback) => {
     // Here we're in the page context. It's like being in your browser's inspector tool
 
     // At the moment, Instagram creates a global js object on media pages called _sharedData which contains the graphql edge data.
     const userEdge = _.get(window._sharedData || {}, 'entry_data.ProfilePage[0].graphql.user', {});
-    const medias = _.get(userEdge, 'edge_owner_to_timeline_media.edges', []);
+    const mediaEdges = _.get(userEdge, 'edge_owner_to_timeline_media.edges', []);
 
-    const data = {
+    const account = {
       id: _.get(userEdge, 'id', null),
       username: _.get(userEdge, 'username', null),
       name: _.get(userEdge, 'full_name', null),
@@ -37,31 +41,37 @@ nick.newTab().then(async (tab) => {
       isPrivate: _.get(userEdge, 'is_private', null),
       isVerified: _.get(userEdge, 'is_verified', null),
       avatar: _.get(userEdge, 'profile_pic_url_hd', null),
-      medias: [],
     };
 
-    _.forEach(medias, function(media) {
-      const mediaEdge = _.get(media, 'node', {});
-      const mediaData = {
-        id: _.get(mediaEdge, 'id', null),
-        shortcode: _.get(mediaEdge, 'shortcode', null),
-        caption: _.get(mediaEdge, 'edge_media_to_caption.edges[0].node.text', null),
-        likesCount: _.get(mediaEdge, 'edge_media_preview_like.count', null),
-        viewsCount: _.get(mediaEdge, 'video_view_count', null),
-        commentsCount: _.get(mediaEdge, 'edge_media_to_comment.count', null),
-        mediaType: _.get(mediaEdge, 'is_video', false) ? 'video' : 'image',
-        imageUrl: _.get(mediaEdge, 'display_url', null),
-        videoUrl: _.get(mediaEdge, 'video_url', null),
-        createdAtTime: _.get(mediaEdge, 'taken_at_timestamp', null),
-      };
-      data.medias.push(mediaData);
-    });
-
-    callback(null, data);
+    callback(null, { owner: account, mediaEdges });
   });
-  
-  await buster.setResultObject(account);
-  console.log(JSON.stringify(account, null, 2));
+})
+.then(async ({ owner, mediaEdges }) => {
+  console.log('Performing data mapping...');
+
+  const medias = [];
+
+  _.forEach(mediaEdges, function(_mediaEdge) {
+    const mediaEdge = _.get(_mediaEdge, 'node', {});
+    const mediaData = {
+      id: _.get(mediaEdge, 'id', null),
+      shortcode: _.get(mediaEdge, 'shortcode', null),
+      caption: _.get(mediaEdge, 'edge_media_to_caption.edges[0].node.text', null),
+      likesCount: _.get(mediaEdge, 'edge_media_preview_like.count', null),
+      viewsCount: _.get(mediaEdge, 'video_view_count', null),
+      commentsCount: _.get(mediaEdge, 'edge_media_to_comment.count', null),
+      mediaType: _.get(mediaEdge, 'is_video', false) ? 'video' : 'image',
+      imageUrl: _.get(mediaEdge, 'display_url', null),
+      videoUrl: _.get(mediaEdge, 'video_url', null),
+      createdAtTime: _.get(mediaEdge, 'taken_at_timestamp', null),
+      owner,
+      isSponsored: Sponsored.isSponsoredMedia(mediaEdge),
+    };
+    medias.push(mediaData);
+  });
+
+  console.log('Data mapping complete!');
+  await buster.setResultObject(medias);
 })
 .then(() => {
   console.log("Job done!");
